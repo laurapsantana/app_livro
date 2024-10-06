69ca349238211a579e913a3014a98bec690e54e9
Laura Santana <111099291+laurapsantana@users.noreply.github.com>
2024-10-06 19:57:09 -0300

    main.dart
    update

--- app_livros/lib/main.dart
+++ app_livros/lib/main.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:app_livros/pages/livros.dart';
 import 'package:app_livros/pages/favoritos.dart';
+import 'package:app_livros/pages/perfil.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App de Livros',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => LivrosPage(),
+        '/': (context) => LivrosPage(toggleTheme: _toggleTheme),
         '/favorites': (context) => FavoritosPage(),
+        '/perfil': (context) => PerfilPage(toggleTheme: _toggleTheme, isDarkMode: isDarkMode),
       },
     );
+  }
+
+  void _toggleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
   }
 }
